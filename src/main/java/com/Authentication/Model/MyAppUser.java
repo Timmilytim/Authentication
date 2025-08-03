9164195779f5074d8a2ee8196f4d8f9be210bd41
Oluwatimilehin Odusanya <oluwapelumiodus@gmail.com>
2025-08-03 21:12:06 +0100

    Final commit on email verification

--- src/main/java/com/Authentication/Model/MyAppUser.java
+++ src/main/java/com/Authentication/Model/MyAppUser.java
@@ -2,8 +2,10 @@ package com.Authentication.Model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Entity
 public class MyAppUser {
 
@@ -11,68 +13,10 @@ public class MyAppUser {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id; // Unique identifier for the user
     private String username; // Username for the user
+    @Column(unique = true, nullable = false)
     private String email; // Email address of the user
     private String password; // Password for the user
-
-    public String getVerificationToken() {
-        return verificationToken;
-    }
-
-    public void setVerificationToken(String verificationToken) {
-        this.verificationToken = verificationToken;
-    }
-
-    public boolean isVerified() {
-        return isVerified;
-    }
-
-    public void setVerified(boolean verified) {
-        isVerified = verified;
-    }
-
-    public String getResetToken() {
-        return resetToken;
-    }
-
-    public void setResetToken(String resetToken) {
-        this.resetToken = resetToken;
-    }
-
     private String verificationToken; // Token for email verification
     private boolean isVerified; // Flag to check if the user is verified
 
-    @Column(name = "reset_token")
-    private String resetToken; // Token for password reset
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
