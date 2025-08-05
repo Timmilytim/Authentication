fc135dd1093874ced97a0e867f1d602b527cb8c8
Oluwatimilehin Odusanya <oluwapelumiodus@gmail.com>
2025-08-05 23:22:44 +0100

    Modified the login field to accept gmail or username

--- src/main/java/com/Authentication/Model/MyAppUserRepository.java
+++ src/main/java/com/Authentication/Model/MyAppUserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MyAppUserRepository extends JpaRepository<MyAppUser, Long> {
-
-    Optional<MyAppUser> findByUsername(String username); // Method to find a user by username
+    Optional<MyAppUser> findByUsernameOrEmail(String username, String email); // Method to find a user by username
     MyAppUser findByEmail(String email); // Method to find a user by email
 }
--- src/main/java/com/Authentication/Service/MyAppUserService.java
+++ src/main/java/com/Authentication/Service/MyAppUserService.java
@@ -19,17 +19,18 @@ public class MyAppUserService implements UserDetailsService {
     @Autowired
     private MyAppUserRepository myAppUserRepository;
 
+    // This method is used by Spring Security to load user details by username
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional <MyAppUser> user = myAppUserRepository.findByUsername(username);
+    public UserDetails loadUserByUsername(String input) throws UsernameNotFoundException {
+        Optional <MyAppUser> user = myAppUserRepository.findByUsernameOrEmail(input, input);
         if(user.isPresent()){
             var userObj = user.get();
             return User.builder()
-                    .username(userObj.getUsername())
-                    .password(userObj.getPassword())
+                    .username(userObj.getUsername()) // If you are getting an error here, just ignore it because I used Lombok's @Getter and @Setter annotations in MyAppUser class
+                    .password(userObj.getPassword())// If you are getting an error here, just ignore it because I used Lombok's @Getter and @Setter annotations in MyAppUser class
                     .build();
         }else {
-            throw new UsernameNotFoundException("User not found with username: " + username);
+            throw new UsernameNotFoundException("User " + input + " not found");
         }
     }
 }
